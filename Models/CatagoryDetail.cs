ad7918ca8bd7d35189ee31ef4c5d392082e3b347
GirHalDev <42422165+GirHalDev@users.noreply.github.com>
2020-03-19 14:17:53 -0500

    Added database, corresponding classes and IRepository

---
+++ Models/CatagoryDetail.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EthioProductShoppingCenter.Models
+{
+    public class CatagoryDetail
+    {
+        public int CatagoryId { get; set; }
+
+        [Required(ErrorMessage = "Catagory name required")]
+        [StringLength(100, ErrorMessage = "Minimium 3 and minimum 5 and maximum 100 characters allowed", MinimumLength = 3)]
+        public string CatagoryName { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+
+    }
+
+    public class ProductDetail
+    {
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product name required")]
+        [StringLength(100, ErrorMessage = "Minimium 3 and minimum 5 and maximum 100 characters allowed", MinimumLength = 3)]
+        public string ProductName { get; set; }
+        [Required]
+        [Range(1, 50)]
+        public Nullable<int> CatagoryId { get; set; }
+        public Nullable<bool> IsActive { get; set; }
+        public Nullable<bool> IsDelete { get; set; }
+        public Nullable<System.DateTime> CreatedDate { get; set; }
+        public Nullable<System.DateTime> ModifiedDate { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public Nullable<System.DateTime> Description { get; set; }
+        public string ProductImage { get; set; }
+        public Nullable<bool> IsFeatured { get; set; }
+
+        [Required]
+        [Range(typeof(int), "1", "500", ErrorMessage = "Invalid quantity")]
+        public Nullable<int> Quantity { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "1", "200000", ErrorMessage = "Invalid price")]
+        public Nullable<decimal> Price { get; set; }
+
+        public SelectList Catagories { get; set; }
+    }
+}
---
+++ Repository/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace EthioProductShoppingCenter.Repository
+{
+    public interface IRepository<tblEntity> where tblEntity: class
+    {
+        IEnumerable<tblEntity> GetAllRecords();
+        IQueryable<tblEntity> GetAllRecordsIQueryable();
+        void Add(tblEntity entity);
+        void Update(tblEntity entity);
+        void UpdateByWhereClause(Expression<Func<tblEntity, bool>> wherePredict, Action<tblEntity> ForEachPredicate);
+        tblEntity GetFirstOrDefault(int recordId);
+        void Remove(tblEntity entity);
+        void RemoveByWhereClause(Expression<Func<tblEntity, bool>> wherePredict);
+        void RemoveRangeByWhereClause(Expression<Func<tblEntity, bool>> wherePredict);
+        void InactiveAndDeleteMarkByWhereClause(Expression<Func<tblEntity, bool>> wherePredict, Action<tblEntity> ForEachPredicate);
+        tblEntity GetFirstOrDefaultByParameter(Expression<Func<tblEntity, bool>> wherePredict);
+        IEnumerable<tblEntity> GetListParameter(Expression<Func<tblEntity, bool>> wherePredict);
+        IEnumerable<tblEntity> GetResultBySqlProcedure(string query, params object[] parameters);
+    }
+}
